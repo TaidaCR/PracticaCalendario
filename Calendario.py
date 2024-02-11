3457bd29f1ce8ac6dd14285163e25a2b451b7419
Taida Carmona <taidacr@hotmail.com>
2024-02-11 11:26:46 +0100

    Creación de archivos iniciales

---
+++ Calendario.py
@@ -0,0 +1,9 @@
+class Dia:
+
+    def __init__(self,dia=1,mes='enero',año=1970,dia_semana='jueves'):
+        self.dia=dia
+        self.mes=mes
+        self.año=año
+        self.dia_semana=dia_semana
+
+    
---
+++ Test_calendario.py
@@ -0,0 +1,3 @@
+from Calendario import Dia:
+
+    
